a4e0c395e6c602863a2611169ae61391aaf739be
Prashanttiwari2190 <77717882+Prashanttiwari2190@users.noreply.github.com>
2023-11-15 15:04:47 +0530

    Add api for cab StyleOptions

---
+++ src/SFM.VehicleBuilder.Domain/Models/CabStyle/ExteriorColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFM.VehicleBuilder.Domain.Models
+{
+    /// <summary>
+    ///   Class for ExteriorColor.
+    /// </summary>
+    public class ExteriorColor
+    {
+        /// <summary>
+        ///   Gets or sets the ColorId.
+        /// </summary>
+        public string ColorId { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the ColorName.
+        /// </summary>
+        public string ColorName { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the ColorCode.
+        /// </summary>
+        public string ColorCode { get; set; }
+    }
+}
