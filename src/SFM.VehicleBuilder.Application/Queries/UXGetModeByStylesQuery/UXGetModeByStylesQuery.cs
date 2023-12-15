199cbc17401587e0f4d686a391fe3c196b191f55
Prashanttiwari2190 <77717882+Prashanttiwari2190@users.noreply.github.com>
2023-12-15 18:57:06 +0530

    updated code for wheel base

---
+++ src/SFM.VehicleBuilder.Application/Queries/UXGetModeByStylesQuery/UXGetModeByStylesQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SFM.VehicleBuilder.Domain.Correlation;
+using SFM.VehicleBuilder.Domain.Models;
+using SFM.VehicleBuilder.Domain.Models.SearchStyle;
+using System.Collections.Generic;
+
+namespace SFM.VehicleBuilder.Application.Queries.UXGetModeByStylesQuery
+{
+    public class UXGetModeByStylesQuery : SecureQueryBase<ModelConfigration>
+    {
+        public UXGetModeByStylesQuery(CorrelationId correlationId)
+            : base(correlationId)
+        {
+        }
+
+        /// <summary>
+        ///   Gets or sets the StyleIds.
+        /// </summary>
+        public int[] StyleIds { get; set; }
+    }
+}
---
+++ src/SFM.VehicleBuilder.Domain/Models/SearchStyle/GenericColors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SFM.VehicleBuilder.Domain.Models
+{
+    /// <summary>
+    ///   A chrome data vehicle GenericColors.
+    /// </summary>
+    public class GenericColors
+    {
+        /// <summary>
+        ///   Gets or sets a value indicating whether gets or sets a chrome data Name.
+        /// </summary>
+        public string[] Name { get; set; }
+
+        /// <summary>
+        ///   Gets or sets a value indicating whether a chrome data RgbValue.
+        /// </summary>
+        public string RgbValue { get; set; }
+    }
+}
